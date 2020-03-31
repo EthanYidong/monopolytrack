9c3cf80761b2d138e85e5a2015d348eb965145a9
Ethan Tang <ethan@ethans-macbook.attlocal.net>
2020-03-30 20:24:06 -0700

    Working state

--- lib/player_drag.dart
+++ lib/player_drag.dart
@@ -9,7 +9,8 @@ class PlayerDrag extends StatefulWidget {
   PlayerDrag({this.color, this.value = 1500, this.setCallback});
 
   @override
-  createState() => PlayerDragState(color: color, amtMoney: value, setCallback: setCallback);
+  createState() =>
+      PlayerDragState(color: color, amtMoney: value, setCallback: setCallback);
 }
 
 class PlayerDragState extends State<PlayerDrag> {
@@ -34,33 +35,36 @@ class PlayerDragState extends State<PlayerDrag> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (context, constraints) => Draggable<void Function(int)>(
-      data: changeMoney,
-      feedback: Container(
-          height: constraints.biggest.height,
-          width: constraints.biggest.width,
-          color: color,
-          child: Center(child: Text(getText(),
-              style: Theme.of(context).textTheme.button))),
-      maxSimultaneousDrags: 1,
-      childWhenDragging: Container(color: Colors.grey),
-      child: DragTarget<void Function(int)>(
-          builder: (BuildContext context, cData, rData) {
-            return Container(
-                color: color,
-                child: Center(
-                  child: Text(getText(),
-                      style: Theme.of(context).textTheme.button),
-                ));
-          },
-          onWillAccept: (data) {
-            return true;
-          },
-          onAccept: (data) {
-            showDialog(context: context, builder: (BuildContext context) {
-              return MoneyDialog(from: data, to: changeMoney);
-            });
-          }),
-    ));
+        builder: (context, constraints) => Draggable<void Function(int)>(
+              data: changeMoney,
+              feedback: Container(
+                  height: constraints.biggest.height,
+                  width: constraints.biggest.width,
+                  color: color,
+                  child: Center(
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: FittedBox(child: Text(getText()))))),
+              maxSimultaneousDrags: 1,
+              childWhenDragging: Container(color: Colors.grey),
+              child: DragTarget<void Function(int)>(
+                  builder: (BuildContext context, cData, rData) {
+                return Container(
+                    color: color,
+                    child: Center(
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: FittedBox(child: Text(getText()))),
+                    ));
+              }, onWillAccept: (data) {
+                return true;
+              }, onAccept: (data) {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return MoneyDialog(from: data, to: changeMoney);
+                    });
+              }),
+            ));
   }
 }
